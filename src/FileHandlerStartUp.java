e5e87d02034dd68081a99dcc91296082300ca082
Johan Settlin <settlin@kth.se>
2020-12-16 14:35:40 +0100

    Done

--- src/FileHandlerStartUp.java
+++ src/FileHandlerStartUp.java
@@ -7,11 +7,13 @@ import java.io.FileNotFoundException;
 public class FileHandlerStartUp {
     public static void main(String[] args) throws Exception {
         FileHandler fh = new FileHandler();
-        String path1 = "/Users/johansettlin/Documents/java-program/payment-file-handler/src/Exempelfil_betalningsservice.txt";//args[0];
-        String path2 = "/Users/johansettlin/Documents/java-program/payment-file-handler/src/Exempelfil_inbetalningstjansten.txt";
-        String path3 = "/Users/johansettlin/Documents/java-program/payment-file-handler/src/Exempelfil_felformat.txt";
-        if(!path1.isEmpty()) {
-            fh.handleFile(path1);
+        String path = args[0];
+        /* For testing *
+        String path1 = "/Users/johansettlin/Documents/java-program/payment-file-handler/Exempelfil_betalningsservice.txt";//args[0];
+        String path2 = "/Users/johansettlin/Documents/java-program/payment-file-handler/Exempelfil_inbetalningstjansten.txt";
+        String path3 = "/Users/johansettlin/Documents/java-program/payment-file-handler/Exempelfil_felformat.txt";*/
+        if(!path.isEmpty()) {
+            fh.handleFile(path);
         }else{
             System.out.println("You need to pass a path to a file to be handled");
         }
